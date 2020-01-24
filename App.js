96ec880f2ca3afc1c6f1d3961ba32a2d99a9c316
Hisnul Hasfan <hisnulhasfan247@gmail.com>
2020-01-24 19:40:10 +0530

    -Changed Background to Gradient -Added Placeholder -Added Styles

--- App.js
+++ App.js
@@ -1,29 +1,72 @@
-import React from 'react';
-import { StyleSheet, Text, View,Button,Alert,Image } from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet,TextInput } from 'react-native';
+import { StatusBar } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
+
+export default class App extends React.Component {
     
-    <Text style={{fontWeight: 'bold'}}> Hisnul Hasfan </Text>
-    <Image source={require('./ir.jpg')} 
-        style={{width: 250, height: 250}}/>
-      <Text>I am Hisnul Hasfan! and I am AMDTian </Text>
-      <Button
-          title="Press me"
-          onPress={() => Alert.alert('Hisnul Hasfan')}
-        />
-        
-    </View>
-  );
+  render() {
+    state = {
+      inputValue: '',
+      todos: []
+    };
+
+    changeText = value => {
+      this.setState({
+        inputValue: value
+      });
+    };
+
+    addItem = () => {
+      if (this.state.inputValue !== '') {
+        this.setState(prevState => {
+          const new ToDo = {
+            titile: this.state.inputValue,
+            createdAt: Date.now(),
+          };
+
+          var todos = this.state.todos.concat(newToDo);
+          this.setState({
+            todos: todos,
+          });
+        });
+      }
+    };
+    return (
+      <LinearGradient colors={['#0F2027','#2C5364']} style={{ flex:1 }}>
+        <StatusBar barStyle="light-content" />
+        <View>
+          <TextInput
+          style={styles.input}
+          placeholder="Type here to add a to do."
+          placeholderTextColor={'#fff'}
+          multiline={true}
+          autoCapitalize="sentences"
+          underlineColorAndroid="transparent"
+          selectionColor={'white'}
+          maxLength={30}
+          returnKeyType="done"
+          autoCorrect={false}
+          blurOnSubmit={true}
+          />
+        </View>
+      </LinearGradient>
+    );
+     
+  
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-
-  },
-});
+const styles = {
+  input:
+  {
+    marginTop: 30,
+    paddingTop: 10,
+    paddingRight: 15,
+    paddingLeft: 15,
+    fontSize: 34,
+    color: 'white',
+    fontweight: '500'
+  }
+}
